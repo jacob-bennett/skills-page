17ed6215bc19815bc00ed22034a91c897f295339
Jacob Bennett <jacob@jacobbennett.tech>
2020-11-25 11:52:20 +0000

    update skills

--- src/data.js
+++ src/data.js
@@ -1,49 +1,57 @@
 module.exports = {
-    "skills": {
-        0: {
-            "name": "Backend",
-            "color": 0,
-            "skills": {
-                "TDD": 100,
-                "SOLID Principles": 100,
-                "PHP": 100,
-                "Symfony": 100,
-                "Agile Methodologies (Kanban, XP, Scrum)": 100,
-                "MySQL": 80,
-                "node.js": 80,
-                "Legacy code": 80,
-                "Microservices": 80,
-                "RabbitMQ": 60,
-                "Python": 40,
-                "Apache Solr": 40,
-                "Elasticseatch": 40,
-                "MongoDB": 20,
-                "Java": 20
-            }
-        },
-        1: {
-            "name": "Frontend",
-            "color": 1,
-            "skills": {
-                "HTML5": 80,
-                "Responsive/Mobile First Design": 80,
-                "CSS": 60,
-                "Javascript": 60,
-                "jQuery": 60,
-                "Bootstrap": 60,
-                "vue.js": 20
-            }
-        },
-        2: {
-            "name": "Devops",
-            "color": 2,
-            "skills": {
-                "Linux (Debian & Red Hat)": 80,
-                "Docker": 80,
-                "AWS": 80,
-                "Apache2": 60,
-                "NGINX": 40
-            }
-        }
+  "skills": {
+    0: {
+      "name": "Backend",
+      "color": 0,
+      "skills": {
+        "Node.js": 100,
+        "TDD": 100,
+        "Legacy Code / Refactoring": 100,
+        "PHP": 80,
+        "MySQL & PostgreSQL": 80,
+        "Design Patterns": 80,
+        "Kafka": 80,
+        "Redis": 80,
+        "RabbitMQ": 60,
+        "Python": 40,
+        "Elasticsearch": 40,
+        "MongoDB": 40,
+        "Java": 20,
+      }
     },
+    1: {
+      "name": "Frontend",
+      "color": 1,
+      "skills": {
+        "HTML5": 80,
+        "CSS & SASS": 80,
+        "Javascript": 80,
+        "Responsive/Mobile First Design": 80,
+        "vue.js": 20,
+      }
+    },
+    2: {
+      "name": "Devops",
+      "color": 2,
+      "skills": {
+        "AWS": 80,
+        "Datadog": 80,
+        "CI/CD": 80,
+        "Docker": 60,
+        "Linux": 60,
+        "New Relic": 60,
+        "NGINX": 40,
+        "Apache2": 40,
+      },
+    },
+    // 3: {
+    //   "name": "Other",
+    //   "color": 2,
+    //   "skills": {
+    //     "Agile Methodologies (Kanban, XP, Scrum)": 100,
+    //     "Git": 100,
+    //     "Microservices": 80,
+    //   },
+    // }
+  },
 };
